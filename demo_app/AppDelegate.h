4e5110639ef125bd6656d59e1b777b42b87cb175
Nicholas Hyatt <hyatt@aether-17.cs.wisc.edu>
2014-02-14 14:54:46 -0600

    Initial Commit

---
+++ demo_app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo_app
+//
+//  Created by Nicholas Hyatt on 2/14/14.
+//  Copyright (c) 2014 Nicholas Hyatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ demo_app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demo_app
+//
+//  Created by Nicholas Hyatt on 2/14/14.
+//  Copyright (c) 2014 Nicholas Hyatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
